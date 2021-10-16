610dcd2a361134363e31ef7cf16e76ba3efe8e07
Juan <jmarin@prexcard.com.ar>
2021-10-16 16:22:19 -0300

    Added Display impl for all enquiry Error types

--- src/enquiry/error.rs
+++ src/enquiry/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Display, Formatter};
+
 /// The Answerer side disconnected and we couldn't ask the question
 #[derive(Debug)]
 pub struct AskError<Q> {
@@ -74,5 +76,46 @@ impl<A> AnswerError<A> {
     }
     pub fn into_inner(self) -> A {
         self.answer
+    }
+}
+
+impl<Q> Display for AskError<Q> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt("Answerer disconnected before question was asked", f)
+    }
+}
+impl<Q> Display for AnswerError<Q> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt("Questioner disconnected before answer was sent", f)
+    }
+}
+impl Display for HearError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt("Answerer disconnected before sending an answer", f)
+    }
+}
+impl Display for HearTimeoutError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HearTimeoutError::Timeout => Display::fmt("Timeout before answer received", f),
+            HearTimeoutError::Disconnected => Display::fmt(&HearError(()), f),
+        }
+    }
+}
+impl<Q> Display for DialogueError<Q> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DialogueError::Ask(a) => Display::fmt(a, f),
+            DialogueError::Hear => Display::fmt(&HearError(()), f),
+        }
+    }
+}
+impl<Q> Display for DialogueTimeoutError<Q> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DialogueTimeoutError::Ask(a) => Display::fmt(a, f),
+            DialogueTimeoutError::Hear => Display::fmt(&HearError(()), f),
+            DialogueTimeoutError::Timeout => Display::fmt(&HearTimeoutError::Timeout, f),
+        }
     }
 }
